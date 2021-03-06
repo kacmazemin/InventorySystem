48c38fd8f0458009a5a57b4fc1ac163dab887275
mehmetemin <kacmazemin72@gmail.com>
2021-03-06 13:04:16 +0300

    feat: create blank ue4 project

---
+++ Source/InventorySystem/InventorySystemGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InventorySystemGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORYSYSTEM_API AInventorySystemGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
