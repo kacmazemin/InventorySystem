896678d2c4892b380b5eb1badf7952babf765ef4
mehmetemin <kacmazemin72@gmail.com>
2021-03-06 18:23:31 +0300

    feat: create DynamicInventoryGrid for GridBase inventory

---
+++ Source/InventorySystem/Inventory/DynamicInventoryGrid.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "DynamicInventoryGrid.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORYSYSTEM_API UDynamicInventoryGrid : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
