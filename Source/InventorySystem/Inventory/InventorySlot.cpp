cb580c2fbe390338952cdf67a0868c640b9cc1bc
mehmetemin <kacmazemin72@gmail.com>
2021-03-11 22:20:55 +0300

    feat: override UserWidget functions for DragDrop operations

--- Source/InventorySystem/Inventory/InventorySlot.cpp
+++ Source/InventorySystem/Inventory/InventorySlot.cpp
@@ -3,3 +3,114 @@
 
 #include "InventorySlot.h"
 
+#include "DragDropWidget.h"
+#include "DynamicInventoryGrid.h"
+#include "Components/GridPanel.h"
+#include "InventoryItemDisplay.h"
+#include "Blueprint/WidgetBlueprintLibrary.h"
+#include "Components/GridSlot.h"
+#include "Blueprint/UserWidget.h"
+#include "Rendering/DrawElements.h"
+
+void UInventorySlot::SetCoordinate(const int RowNo, const int ColumnNo)
+{
+	Row = RowNo;
+	Column = ColumnNo;
+}
+
+bool UInventorySlot::NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent,
+                                  UDragDropOperation* InOperation)
+{
+	Super::NativeOnDrop(InGeometry, InDragDropEvent, InOperation);
+
+	UDragDropWidget* DropWidget = Cast<UDragDropWidget>(InOperation);
+	if (DropWidget == nullptr)
+	{
+		GEngine->AddOnScreenDebugMessage(-1,1.f, FColor::Black, "FALSE");
+		return false;
+	}
+	
+	
+	const FString Message = FString::Printf(TEXT("ROW [%d] COLUMN [%d]"), Row, Column);
+	GEngine->AddOnScreenDebugMessage(-1,1.f, FColor::Black, Message);
+
+	if(UGridPanel* GridPanel = Cast<UGridPanel>(GetParent()))
+	{
+		if(UInventoryItemDisplay* InventoryItemDisplay = Cast<UInventoryItemDisplay>(DropWidget->WidgetToDrag))
+		{
+			InventoryItemDisplay->SetVisibility(ESlateVisibility::Visible);
+			if(UGridSlot* GridSlot = Cast<UGridSlot>(InventoryItemDisplay->Slot))
+			{
+				GridSlot->SetColumn(Column);
+				GridSlot->SetRow(Row);
+			}
+		}
+	}
+
+	
+	canDraw = false;
+
+	return true;
+}
+
+bool UInventorySlot::NativeOnDragOver(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent,
+	UDragDropOperation* InOperation)
+{
+	Super::NativeOnDragOver(InGeometry, InDragDropEvent, InOperation);
+
+	UDragDropWidget* DropWidget = Cast<UDragDropWidget>(InOperation);
+	
+	if (DropWidget == nullptr)
+	{
+		return false;
+	}
+
+	if(UGridPanel* GridPanel = Cast<UGridPanel>(GetParent()))
+	{
+		const FString Message = FString::Printf(TEXT("ROW [%d] COLUMN [%d]"), Row, Column);
+		GEngine->AddOnScreenDebugMessage(-1,1.f, FColor::Black, Message);
+
+		canDraw = true;
+	}
+
+	return true;
+}
+
+int32 UInventorySlot::NativePaint(const FPaintArgs& Args, const FGeometry& AllottedGeometry,
+                                  const FSlateRect& MyCullingRect, FSlateWindowElementList& OutDrawElements, int32 LayerId,
+                                  const FWidgetStyle& InWidgetStyle, bool bParentEnabled) const
+{
+	Super::NativePaint(Args, AllottedGeometry, MyCullingRect, OutDrawElements, LayerId, InWidgetStyle, bParentEnabled);
+	
+	if ( canDraw )
+	{
+		FPaintContext Context(AllottedGeometry, MyCullingRect, OutDrawElements, LayerId, InWidgetStyle, bParentEnabled);
+		OnPaint( Context );
+
+		if(Owner)
+		{
+			const bool IsSlotValid = Owner->IsItemAvailableForSlot();
+		}
+		
+		UWidgetBlueprintLibrary::DrawBox(Context, {0,0}, {100,100}, BrushAsset, ValidPlaceColor);
+		
+	
+		return FMath::Max(LayerId, Context.MaxLayer);
+	}
+
+	return LayerId;
+}
+
+void UInventorySlot::NativeOnDragLeave(const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation)
+{
+	Super::NativeOnDragLeave(InDragDropEvent, InOperation);
+
+	canDraw = false;
+}
+
+void UInventorySlot::NativeOnDragCancelled(const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation)
+{
+	Super::NativeOnDragCancelled(InDragDropEvent, InOperation);
+
+	canDraw = false;
+}
--- Source/InventorySystem/Inventory/InventorySlot.h
+++ Source/InventorySystem/Inventory/InventorySlot.h
@@ -6,6 +6,8 @@
 #include "Blueprint/UserWidget.h"
 #include "InventorySlot.generated.h"
 
+class USlateBrushAsset;
+class UDynamicInventoryGrid;
 /**
  * 
  */
@@ -13,5 +15,39 @@ UCLASS()
 class INVENTORYSYSTEM_API UInventorySlot : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	void SetCoordinate(const int RowNo, const int ColumnNo);
+
+	UPROPERTY(EditDefaultsOnly)
+	USlateBrushAsset* BrushAsset = nullptr;
+
+	UPROPERTY(EditDefaultsOnly)
+	FLinearColor ValidPlaceColor;
+
+	UPROPERTY(EditDefaultsOnly)
+	FLinearColor InvalidPlaceColor;
+
+	UPROPERTY()
+	UDynamicInventoryGrid* Owner = nullptr;
+	
+private:
+
+	bool canDraw = false;
+	int Index = -1;
+	int Row = -1;
+	int Column = -1;
+	
+	virtual bool NativeOnDrop( const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation ) override;
+	virtual bool NativeOnDragOver( const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation ) override;
+
+	virtual int32 NativePaint(const FPaintArgs& Args, const FGeometry& AllottedGeometry, const FSlateRect& MyCullingRect,
+							  FSlateWindowElementList& OutDrawElements, int32 LayerId,
+							  const FWidgetStyle& InWidgetStyle, bool bParentEnabled ) const override;
+	
+	virtual void NativeOnDragLeave( const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation ) override;
+	virtual void NativeOnDragCancelled( const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation ) override;
+
+
 	
 };
