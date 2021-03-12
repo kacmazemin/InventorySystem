016ca94eb63114781d7907f250f49c3857c8f3d3
mehmetemin <kacmazemin72@gmail.com>
2021-03-12 18:53:49 +0300

    feat: create InventoryComponent to hold current inventory data

---
+++ Source/InventorySystem/Inventory/InventoryComponent.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "InventoryComponent.generated.h"
+
+class UBasicItemDataAsset;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class INVENTORYSYSTEM_API UInventoryComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UInventoryComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:
+
+	TArray<UBasicItemDataAsset*> ItemContainer;
+	
+};
