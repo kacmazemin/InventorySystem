883d819a27d435587870974b085ba8a27d1f5079
mehmetemin <kacmazemin72@gmail.com>
2021-03-12 23:21:52 +0300

    feat: create actor class for ItemData

---
+++ Source/InventorySystem/Item/BaseItemActor.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "GameFramework/Actor.h"
+#include "BaseItemActor.generated.h"
+
+UCLASS()
+class INVENTORYSYSTEM_API ABaseItemActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABaseItemActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
